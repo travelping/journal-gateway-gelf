0ffe704a575588f821433de30d0ea724c3f15824
Tobias Hintze <thintze+git@tpip.net>
2015-10-01 17:04:10 +0200

    remove unused parts, rename remaining parts

--- src/journal-gateway-zmtp-control.h
+++
@@ -1,7 +0,0 @@
-/* definitions for controlling communication */
-#define CTRL_LOGON "\001"	// initialise connection
-#define CTRL_READY "\002"	// signal that initiation of connection finished
-#define CTRL_LOGOFF "\003"	// stop connection
-#define CTRL_UKCOM "\005" // signals that the received command was not understood (unknown)
-#define CTRL_ACCEPTED "\006" // signals the the received command was accepted
-#define DEFAULT_CONTROL_TARGET "tcp://127.0.0.1:27001"
